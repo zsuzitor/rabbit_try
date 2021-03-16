2fbb051b76fb0318ccd02dc18bed2fbf0a731c45
zsuzitor <Avdosevstas@mail.ru>
2021-03-16 19:10:52 +0300

    init

---
+++ ProducerSimple/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ProducerSimple
+{
+    class Program
+    {
+        //install https://www.erlang.org/downloads
+        //install https://www.rabbitmq.com/install-windows.html
+
+        //https://github.com/rabbitmq/rabbitmq-tutorials/blob/master/dotnet
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
